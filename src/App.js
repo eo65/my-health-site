f973bf4e1e0c46216919474a4fa339db27d6eab4
eo65 <35512966+eo65@users.noreply.github.com>
2019-05-29 14:14:48 -0400

    adding react router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+ 
+class Main extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HashRouter>
+        <div>
+          <h1>My Cars Sales</h1>
+          <ul className="header">
+            <li>/*<NavLink to="/">*/Home/*</NavLink>*/</li>
+            <li>/*<NavLink to="/">*/List/*</NavLink>*/</li>
+          </ul>
+          /*<div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/List" component={List}/>
+			<Route path="/Luxury" component={Luxury}/>
+          </div>*/
+        </div>
+      </HashRouter>
     );
   }
 }
 
-export default App;
+export default Main;
